5344ff3b7a22c62469ef34f3de89bc3d55954313
Billy Grossman <billygrossman@BillysMBP142022.fios-router.home>
2022-04-06 09:52:45 -0400

    BG-3: Content updates

--- src/data/content.js
+++ src/data/content.js
@@ -28,7 +28,7 @@ export const content = {
         content: `I am a full stack developer focusing more on frontend technology and user facing applications. I also enjoy API design and cloud architecture.`,
         details: [
           'Highly skilled in creating performant <span class="text--tag">HTML</span>, <span class="text--tag">CSS</span>',
-          'Experienced with JavaScript, Frameworks (<span class="text--tag">Vue</span>, <span class="text--tag">Nuxt</span>, <span class="text--tag">React</span>) and Templating Languages (<span class="text--tag">Pug</span>, <span class="text--tag">Jinja2</span>)',
+          'Experienced with JavaScript Frameworks (<span class="text--tag">Vue</span>, <span class="text--tag">Nuxt</span>, <span class="text--tag">React</span>) and Templating Languages (<span class="text--tag">Pug</span>, <span class="text--tag">Jinja2</span>)',
           'Passionate about <span class="text--tag">CSS</span> Methodologies (I lean to <span class="text--tag">BEM</span>) and CSS Preprocessors (leaning hard at <span class="text--tag">SCSS</span>)',
           'Proficient with Task Runners / Build Tools (<span class="text--tag">WebPack</span>, <span class="text--tag">Grunt</span>, <span class="text--tag">Gulp</span>) and Package Managers (<span class="text--tag">NPM</span>, <span class="text--tag">Yarn</span>, <span class="text--tag">Composer</span>) and <span class="text--tag">Git</span> Version Control Systems',
           'Skilled in creating Design Systems, <span class="text--tag">Component Libraries</span>, Prototypes and Style Guides',
@@ -46,10 +46,10 @@ export const content = {
           <h3>This Website</h3>
           <p>
             The source code for this personal web application <a href="https://github.com/billygrossman/billygrossman.com" target="_blank">
-            is available on Github</a>. It's a failry simple application, built in <span class="text--tag">Vue</span> and deployed to AWS S3.
+            is available on Github</a>. It's a fairly simple application, built using <span class="text--tag">Vue</span> and deployed to <span class="text--tag">AWS S3</span>.
             I will continue to make improvements and updates to this application as it can serve as a clean frontend boilerplate.
           </p>
-          <p>You can also check out the <a href="/components">Design Guide/Component library</a> to see how I like to structure Vue components.</p>
+          <p>You can also check out the <a href="/components">Design Guide/Component library</a> to see how I like to structure <span class="text--tag">Vue</span> components.</p>
           <p>Here are some features that are on deck:</p>
         `,
         details: [
